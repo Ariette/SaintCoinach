cc8c7794e52a279736b8b805a2e3af0e828a008e
Ariette <love.mary123@gmail.com>
2022-10-06 00:26:41 +0900

    Add SubmarineExploration

---
+++ SaintCoinach/Xiv/SubmarineExploration.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaintCoinach.Xiv {
+    public class SubmarineExploration  : XivRow {
+        #region Properties
+
+        public Text.XivString Name { get { return AsString("Destination"); } }
+
+        public int RequiredLevel { get { return AsInt32("RankReq"); } }
+
+        public int RequiredFuel { get { return AsInt32("CeruleumTankReq"); } }
+
+        public TimeSpan Duration { get { return TimeSpan.FromMinutes(AsInt32("SurveyDuration<min>")); } }
+
+        public int ExpReward { get { return AsInt32("ExpReward"); } }
+
+        #endregion
+
+        #region Constructors
+
+        public SubmarineExploration(IXivSheet sheet, SaintCoinach.Ex.Relational.IRelationalRow sourceRow) : base(sheet, sourceRow) { }
+
+        #endregion
+    }
+}
